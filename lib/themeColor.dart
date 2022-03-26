3835a5bd533cf479287f30282e1bd12272cc125b
wa765 <wahyusaputra12345678@gmail.com>
2022-03-26 15:21:40 +0700

    Color Style

---
+++ lib/themeColor.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Purple Color
+Color purpleColor = const Color(0xff6D54B5);
+
+// Dark Color
+Color darkColor = const Color(0xff000000);
+
+// Grey Color
+Color greyColor = const Color(0xff908989);
+
+// White Color
+Color whiteColor = Colors.white;
+
+// Ligth Gray  Color
+Color ligthGrayColor = const Color(0xffE7E7F6);
+
+// 
