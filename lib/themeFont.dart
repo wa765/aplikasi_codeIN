5c603f115259ae7d77fd3aa70f418e3a7c8cd189
wa765 <wahyusaputra12345678@gmail.com>
2022-03-26 15:22:07 +0700

    Font Style

---
+++ lib/themeFont.dart
@@ -0,0 +1,25 @@
+import 'package:aplikasi_codein/themeColor.dart';
+import 'package:flutter/material.dart';
+
+// Main Heading
+TextStyle mainHeading = TextStyle(
+  color: darkColor,
+  fontSize: 25,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.05,
+);
+
+// Secondary Heading
+TextStyle secondaryHeading = TextStyle(
+  color: darkColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.05,
+);
+
+// Paragraf
+TextStyle paragraph = TextStyle(
+  color: greyColor,
+  fontSize: 16,
+  letterSpacing: 0.5,
+);
